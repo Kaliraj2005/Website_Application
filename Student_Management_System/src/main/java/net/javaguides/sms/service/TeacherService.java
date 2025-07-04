acb2975f746a4703e5e07f97b468db315c792c95
Kaliraj T <kalirajk217@gmail.com>
2025-07-04 23:45:18 +0530

    Add files via upload

---
+++ Student_Management_System/src/main/java/net/javaguides/sms/service/TeacherService.java
@@ -0,0 +1,17 @@
+package net.javaguides.sms.service;
+
+import java.util.List;
+
+import net.javaguides.sms.entity.Teacher;
+
+public interface TeacherService {
+	List<Teacher> getAllTeachers();
+	
+	Teacher saveTeacher(Teacher teacher);
+	
+	Teacher getTeacherById(Long id);
+	
+	Teacher updateTeacher(Teacher teacher);
+	
+	void deleteTeacherById(Long id);
+}
